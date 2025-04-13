e55010907710f2c9462e910d5cfe7e23e18c6721
Матвей <matvey.myown@gmail.com>
2025-04-13 21:50:48 +0300

    Fix logic.cpp in 4th task

--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -1,65 +1,66 @@
-﻿// Количество локальных минимумов и максимумов
-// [The Number of local minima and maxima]
-//
-// Дан вектор целочисленных значений. Необходимо разработать
-// функцию, которая подсчитывает количество локальных минимумов 
-// и максимумов.
-#include "logic.h"
+﻿#include "logic.h"
 
 int count_local_max(int* array, int size)
 {
-	int count = 0;
+    if (size < 2)
+    {
+        return 0;
+    }
 
-	if (*(array) > *(array + 1) && size > 1)
-	{
-		count++;
-	}
+    int count = 0;
 
-	if (*(array + size - 1) > * (array + size - 2) && size > 1)
-	{
-		count++;
-	}
+    if (array[0] > array[1])
+    {
+        count++;
+    }
 
-	for (int i = 1; i < size - 2; i++)
-	{
-		if (*(array + i) > *(array + i + 1) &&
-			*(array + i) > *(array + i - 1))
-		{
-			count++;
-		}
-	}
+    if (array[size - 1] > array[size - 2])
+    {
+        count++;
+    }
 
-	return count;
+    for (int i = 1; i < size - 1; i++)
+    {
+        if (array[i] > array[i - 1] && array[i] > array[i + 1])
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 int count_local_min(int* array, int size)
 {
-	int count = 0;
+    if (size < 2)
+    {
+        return 0;
+    }
 
-	if (*(array) < * (array + size) && size > 1)
-	{
-		count++;
-	}
+    int count = 0;
 
-	if (*(array + size - 1) < * (array + size - 2) && size > 1)
-	{
-		count++;
-	}
+    if (array[0] < array[1])
+    {
+        count++;
+    }
 
-	for (int i = 1; i < size - 2; i++)
-	{
-		if (*(array + i) < * (array + i + 1) &&
-			*(array + i) < * (array + i - 1))
-		{
-			count++;
-		}
-	}
+    if (array[size - 1] < array[size - 2])
+    {
+        count++;
+    }
 
-	return count;
+    for (int i = 1; i < size - 1; i++)
+    {
+        if (array[i] < array[i - 1] && array[i] < array[i + 1])
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 void count_local_min_and_max(int* array, int size, int* min_count, int* max_count) {
-
-	*min_count = count_local_min(array, size);
-	*max_count = count_local_max(array, size);
+    *min_count = count_local_min(array, size);
+    *max_count = count_local_max(array, size);
 }
